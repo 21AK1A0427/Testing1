0719036b5921d2792dae6b0627b9894e0d962e91
gangadhar517 <131477571+gangadhar517@users.noreply.github.com>
2025-05-26 16:00:35 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'your-username' and 'your-repo' with your actual GitHub username and repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // important for GitHub Pages
+  base: 'Testing1', // important for GitHub Pages
 })
